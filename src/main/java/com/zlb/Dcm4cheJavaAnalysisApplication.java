a981258bae7645c2b3b1ee336fc38b595ae522bf
ozzsasx <2390172493@qq.com>
2025-07-25 14:51:50 +0800

    feat(core): 添加DICOM生成、读取和修改功能

---
+++ src/main/java/com/zlb/Dcm4cheJavaAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.zlb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dcm4cheJavaAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dcm4cheJavaAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zlb/dcm4chejavaanalysis/Dcm4cheJavaAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlb.dcm4chejavaanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dcm4cheJavaAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
